09f790901aab1015d103f7e7dabdbd2b15a37d68
midorishibukawa <shibukawa.midori@gmail.com>
2021-10-06 09:17:02 -0300

    first visual mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
