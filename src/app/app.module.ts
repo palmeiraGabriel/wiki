fcbe37c88b49e637c6c00784fd90632e31287360
midorishibukawa <shibukawa.midori@gmail.com>
2021-10-07 09:15:26 -0300

    minor visual adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -23,6 +24,7 @@ import { HomeComponent } from './components/pages/home/home.component';
     HomeComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  text: string[] = '「 緑 」 wiki.midori.dev'.split(' ');
+  logo: string = '';
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  writeText(): void {
+    this.logo = this.text.reduce(this.addTags);
+  }
+
+  addTags(prevText: string, curText: string): string {
+      if (prevText == '「' || prevText == '」') return '<b class="alt">' + prevText + '</b>' + curText;
+      if (curText == '「' || curText == '」') return prevText + '<b class="alt">' + curText + '</b>';
+      return prevText + curText;
+    }
+
+  ngOnInit(): void {
+    this.writeText();
+  }
 }
